17ddd7bdb8384a098b564d3614fd1f509ba1b0c9
Muhammad Saad <msaad.bscs22seecs@seecs.edu.pk>
2023-10-11 12:16:53 -0700

    firebase setup

---
+++ lib/types/firebase.ts
@@ -0,0 +1,22 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDw9v9kFbvcZnpXJH3nTKDAFIj20J_NPIY",
+  authDomain: "twitter-b5801.firebaseapp.com",
+  projectId: "twitter-b5801",
+  storageBucket: "twitter-b5801.appspot.com",
+  messagingSenderId: "288971366763",
+  appId: "1:288971366763:web:24e3dd1c5a5246a6daac2e",
+  measurementId: "G-L159LTNH05"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const firebaseAuth = getAuth(app);
